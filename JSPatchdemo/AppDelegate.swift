124758eb8e254f21e1dfece59d4ecac9e989e8dd
yanshi <yanshi_hu@hiersun.com>
2016-11-04 15:40:35 +0800

    修正文档

--- JSPatchdemo/AppDelegate.swift
+++ JSPatchdemo/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        JPEngine.startEngine()
+        let sourcePath = NSBundle.mainBundle().pathForResource("demo", ofType: "js")
+        let script = try! String.init(contentsOfFile: sourcePath!, encoding: NSUTF8StringEncoding)
+        JPEngine.evaluateScript(script)
         // Override point for customization after application launch.
         return true
     }
